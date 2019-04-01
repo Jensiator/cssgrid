d8bd48a85cc1c589f811c712b7930b235e35419f
Jens Alenius <jens.alenius@gmail.com>
2019-04-01 13:47:52 +0200

    Fixed problem

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ModalComponent } from './modal/modal';
 import { ModalService } from './modal/modal.service';
-import { ConfirmModal } from './modal/confirm.modal';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        ModalComponent,
-        ConfirmModal
+        ModalComponent
     ],
     exports: [
-        ModalComponent,
-        ConfirmModal
+        ModalComponent
     ],
     providers: [ModalService]
 })
